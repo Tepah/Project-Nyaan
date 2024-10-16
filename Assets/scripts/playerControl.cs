f9bb11ff8ea4d41121bf018f6591b02c10ba23d0
Douglasmyu <douglasmyu@csu.fullerton.edu>
2024-10-16 16:59:01 -0700

    animations

--- Assets/scripts/playerControl.cs
+++ Assets/scripts/playerControl.cs
@@ -2,9 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Events;
 
 public class PlayerControl : MonoBehaviour
 {
+    public Animator animator;
     private float horizontal;
     private float speed = 8f;
     private float jumpingPower = 10f;
@@ -12,33 +14,60 @@ public class PlayerControl : MonoBehaviour
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private Transform groundCheck;
     [SerializeField] private LayerMask groundLayer;
+    [Header("Events")]
+	[Space]
+
+    public UnityEvent OnLandEvent;
+    public class BoolEvent : UnityEvent<bool> { }
+
+
 
     void Update()
     {
         horizontal = Input.GetAxisRaw("Horizontal");
+        animator.SetFloat("speed", Mathf.Abs(horizontal));
 
         if (Input.GetButtonDown("Jump") && isGrounded())
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
+            animator.SetBool("isJumping", true);
         }
         if (Input.GetButtonUp("Jump") && rb.velocity.y > 0f)
         {
             rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
+            
         }
 
         Flip();
         quitGame();
     }
 
+    public void Awake()
+    {
+        if(OnLandEvent == null)
+            OnLandEvent = new UnityEvent();
+    }
+    public void onLanding()
+    {
+        animator.SetBool("isJumping", false);
+        OnLandEvent.Invoke();
+    }
+
     private void FixedUpdate()
     {
         rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+
+        if (isGrounded() && animator.GetBool("isJumping"))
+        {
+            onLanding();
+        }
     }
 
     private bool isGrounded() // Change void to bool
     {
         // Return true if the groundCheck overlaps with the groundLayer
         return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+
     }
 
     private void Flip()
@@ -51,6 +80,7 @@ public class PlayerControl : MonoBehaviour
             transform.localScale = localScale;
         }
     }
+
     private void quitGame()
     {
         if (Input.GetKeyDown(KeyCode.Q))
