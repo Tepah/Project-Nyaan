e35f493b5fcb2fd1f0a38a59acf48d0e39eae981
Tepah <67202848+Tepah@users.noreply.github.com>
2024-10-16 00:16:15 -0700

    Volume settings work and pull from Player Prefs

--- Assets/scripts/PauseMenu.cs
+++ Assets/scripts/PauseMenu.cs
@@ -2,11 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PauseMenu : MonoBehaviour
 {
     [SerializeField] GameObject pauseMenu;
+    [SerializeField] GameObject settingsMenu;
+    [SerializeField] Slider backgroundSlider;
+    [SerializeField] Slider itemSlider;
     public AudioSource backgroundAudio;
+    public AudioSource itemSound;
 
     public void Pause() //pauses the game when clicked
     {   
@@ -28,8 +33,59 @@ public class PauseMenu : MonoBehaviour
         backgroundAudio.UnPause();
     }
 
-    public void Settings() //control volume
+    public void Settings()
     {
-        
+        pauseMenu.SetActive(false);
+        settingsMenu.SetActive(true);
+        LoadVolume();
+    }
+
+    public void LoadVolume()
+    {
+        if (PlayerPrefs.HasKey("bgVolume"))
+        {
+            Debug.Log("bgVolume has key");
+            float volume = PlayerPrefs.GetFloat("bgVolume");
+            backgroundSlider.value = volume;
+            AdjustBackgroundVolume(volume);
+        } else {
+            float volume = backgroundAudio.volume;
+            backgroundSlider.value = volume;
+        }
+        if (PlayerPrefs.HasKey("fxVolume"))
+        {
+            Debug.Log("fxVolume has key");
+            float volume = PlayerPrefs.GetFloat("fxVolume");
+            itemSound.volume = volume;
+            AdjustItemVolume(volume);
+        } else {
+            float volume = itemSound.volume;
+            itemSlider.value = volume;
+        }
+    }
+
+    public void SaveVolume()
+    {
+        PlayerPrefs.SetFloat("bgVolume", backgroundSlider.value);
+        PlayerPrefs.SetFloat("fxVolume", itemSound.volume);
+    }
+
+    public void AdjustBackgroundVolume(float volume)
+    {
+        backgroundAudio.volume = volume;
+    }
+
+    public void AdjustItemVolume(float volume)
+    {
+        itemSound.volume = volume;
+    }
+
+    public void CloseSettings() 
+    {
+        AdjustBackgroundVolume(backgroundSlider.value);
+        AdjustItemVolume(itemSlider.value);
+        SaveVolume();
+        settingsMenu.SetActive(false);
+        pauseMenu.SetActive(true);
     }
 }
